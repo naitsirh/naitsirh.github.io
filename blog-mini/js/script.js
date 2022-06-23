6d296dea68f5ed8a088549878532584f3ece9dfd
naitsirh <79968809+naitsirh@users.noreply.github.com>
2022-06-23 07:17:30 -0300

    Add files via upload

---
+++ blog-mini/js/script.js
@@ -0,0 +1,115 @@
+// Botón de desplegar menú
+
+document.getElementById('icon-menu').addEventListener('click', mostrar_menu);
+
+function mostrar_menu(){
+  document.getElementById('move-content').classList.toggle('move-container-all');
+  document.getElementById('show-menu').classList.toggle('show-lateral');
+}
+
+
+
+// Scroll up - botón arriba
+// ====== == = ===== ======
+
+// Código de clic del botón
+document.getElementById('button-up').addEventListener('click', scrollUp);
+
+function scrollUp(){
+  var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if(currentScroll > 0){
+    window.requestAnimationFrame(scrollUp);
+    window.scrollTo(0, currentScroll - (currentScroll / 10));
+    //buttonUp.style.transform='scale(0)';
+    // ^--supuestamente para que desaparezca rápido el botón
+  }
+}
+
+// Código del scroll para aparecer y desaparecer el botón
+buttonUp = document.getElementById('button-up');
+
+window.onscroll = function(){
+  var scroll = document.documentElement.scrollTop;
+
+  if(scroll > 500){
+    buttonUp.style.transform='scale(1)';
+  }else if(scroll < 500){
+    buttonUp.style.transform='scale(0)';
+  }
+}
+
+
+
+// Buscador de contenido
+// ======== == =========
+
+// Ejecución de las funciones
+document.getElementById('icon-search').addEventListener('click', mostrar_buscador);
+document.getElementById('cover-ctn-search').addEventListener('click', ocultar_buscador);
+
+// Declarando variables
+bars_search = document.getElementById('ctn-bars-search');
+cover_ctn_search = document.getElementById('cover-ctn-search');
+inputSearch = document.getElementById('inputSearch');
+box_search = document.getElementById('box-search');
+
+// Función para mostrar el buscador
+function mostrar_buscador(){
+  bars_search.style.top='80px';
+  cover_ctn_search.style.display='block';
+  inputSearch.focus();
+
+  if(inputSearch.value===''){
+    box_search.style.display='none';
+  }
+}
+
+// Función para ocultar el buscador
+function ocultar_buscador(){
+  bars_search.style.top='-10px';
+  cover_ctn_search.style.display='none';
+  inputSearch.value='';
+  box_search.style.display='none';
+}
+
+
+
+// Ejecución de código al presionar una tecla
+// (ocultar barra de búsqueda al presionar 'Esc')
+function presionar_tecla(){
+  tecla_esc = event.keyCode;
+
+  if(tecla_esc == 27){
+    ocultar_buscador();  // el ejercicio iba con 'return'
+  }
+}
+
+window.onkeydown = presionar_tecla;
+
+
+
+// Creación de filtrado de búsqueda
+document.getElementById('inputSearch').addEventListener('keyup', buscador_interno);
+
+function buscador_interno(){
+  filter = inputSearch.value.toUpperCase();
+  li = box_search.getElementsByTagName('li');
+
+  // Reccorriendo los elementos a filtrar mediante los 'li'
+  for(i=0; 0<li.length; i++){
+    a = li[i].getElementsByTagName('a')[0];
+    textValue = a.textContent || a.innerText;
+
+    if(textValue.toUpperCase().indexOf(filter) > -1){
+      li[i].style.display='';
+      box_search.style.display='block';
+
+      if(inputSearch.value===''){
+        box_search.style.display='none';
+      }
+    }else{
+      li[i].style.display='none';
+    }
+  }
+}
